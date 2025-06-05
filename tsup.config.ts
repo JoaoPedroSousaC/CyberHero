f34f2d77020ea64fe7aea4df1171ff02b615019d
Gabriel-BarbosaDSB <gabriel.brbs84@gmail.com>
2025-06-05 12:45:02 -0300

    Deploy

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/server.ts'],
   outDir: 'dist',
-  target: 'es2020',
+  target: 'node20',
   format: ['cjs'],
-  splitting: false,
   clean: true,
-  dts: true,
+  sourcemap: true,
+  dts: false,
+  skipNodeModulesBundle: true,
 });
