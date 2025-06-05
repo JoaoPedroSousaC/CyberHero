7868742362b88956fa1c66b1aa1b1ce09f3daf8a
Gabriel-BarbosaDSB <gabriel.brbs84@gmail.com>
2025-06-05 14:26:29 -0300

    Deploy

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/server.ts'],
-  format: ['esm'],
-  target: 'node20',
   outDir: 'dist',
-  clean: true,
+  target: 'node20',
+  format: ['esm'],
+  splitting: false,
+  sourcemap: true,
   outExtension: () => ({ js: '.js' })
 });
