9fd194dd292007dc041e13bb791fd134f1e25e60
Vinícius Belo <115327016+ViniciusBelo@users.noreply.github.com>
2025-06-05 16:52:33 -0300

    vOLTANDO

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
 import fastify, { FastifyInstance } from "fastify";
-import cors from '@fastify/cors';
-
 import { userRoutes } from "./routes/user.routes";
 import { administradorRoutes } from "./routes/administrador.routes";
 import { JogoRoutes } from "./routes/jogo.routes";
@@ -9,31 +7,51 @@ import { ConteudoImagemRoutes } from "./routes/conteudoimagem.routes";
 import { LojaRoutes } from "./routes/loja.routes";
 import { MedalhaRoutes } from "./routes/medalha.routes";
 import { MedalhaDisponivelNaLojaRoutes } from "./routes/medalhadisponivelnaloja.routes";
+const app: FastifyInstance = fastify({})
 
-const app: FastifyInstance = fastify({});
+app.register(userRoutes, {
+    prefix: '/users',
+    
+})
 
-// Habilita CORS
-await app.register(cors, {
-  origin: true, // ou especifique: ['http://localhost:3000', 'https://seu-front.vercel.app']
-});
+app.register(administradorRoutes, {
+    prefix: '/administrador',
+    
+})
 
-app.register(userRoutes, { prefix: '/users' });
-app.register(administradorRoutes, { prefix: '/administrador' });
-app.register(JogoRoutes, { prefix: '/jogo' });
-app.register(ConteudoEducativoRoutes, { prefix: '/conteudoeducativo' });
-app.register(ConteudoImagemRoutes, { prefix: '/conteudoimagem' });
-app.register(LojaRoutes, { prefix: '/loja' });
-app.register(MedalhaRoutes, { prefix: '/medalha' });
-app.register(MedalhaDisponivelNaLojaRoutes, { prefix: '/medalhadisponivelnaloja' });
+app.register( JogoRoutes, {
+    prefix: '/jogo',
+    
+})
+
+app.register(ConteudoEducativoRoutes, {
+    prefix: '/conteudoeducativo',
+    
+})
+
+app.register(ConteudoImagemRoutes, {
+    prefix: '/conteudoimagem',
+    
+})
+
+app.register(LojaRoutes, {
+    prefix: '/loja',
+    
+})
+
+app.register(MedalhaRoutes, {
+    prefix: '/medalha',
+    
+})
+
+app.register(MedalhaDisponivelNaLojaRoutes, {
+    prefix: '/medalhadisponivelnaloja',
+    
+})
 
 const PORT = process.env.PORT || 3000;
 
 app.listen({
-  port: Number(PORT),
-  host: '0.0.0.0',
-}).then(() => {
-  console.log(`🚀 Server is running on port ${PORT}`);
-}).catch((err) => {
-  console.error(err);
-  process.exit(1);
+  port: Number(process.env.PORT) || 3000,
+  host: '0.0.0.0'
 });
