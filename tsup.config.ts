fea68d103bc17d8118d261bb17f3bdfee24703fd
Gabriel-BarbosaDSB <gabriel.brbs84@gmail.com>
2025-06-05 13:00:19 -0300

    Deploy

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,15 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/server.ts'],
   outDir: 'dist',
+  format: ['cjs'],
   splitting: false,
   sourcemap: true,
   clean: true,
   target: 'node20',
-  format: ['cjs'],
-  dts: true,
-  exclude: ['src/generated/**/*', '**/*.prisma', '**/*.d.ts', '**/*.node'],
+  dts: false,
+  shims: false,
+  esbuildOptions(options) {
+    options.external ||= [];
+    options.external.push('./generated/prisma/**', 'prisma', '@prisma/client');
+  }
 });
